a32f9f54c3de5822652151440f08a40d9f5f532d
lcirne <lcirne@uvm.edu>
2026-04-03 15:23:09 -0400

    Removing output from directory.

--- containers/container-2/queue_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --nodes=1                  # Number of nodes
-#SBATCH --time=24:00:00             # Wall clock time
-#SBATCH --ntasks-per-node 20
-#SBATCH --partition gpu
- 
-python3 wrapper.py <scan_inputs.txt
