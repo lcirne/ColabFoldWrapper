98c15ba9291cacbb769960d595c8a83781a55f82
lcirne <lcirne@uvm.edu>
2026-04-09 16:05:20 -0400

    Fixing hardcoded filepath for mod_counts.json.

--- wrapper.py
+++ wrapper.py
@@ -15,7 +15,7 @@ import numpy as np
 import data_engine as engine
 
 # ---- Global Variables ----
-MOD_COUNTS_FILEPATH = "~/scratch/mod_counts.json"
+MOD_COUNTS_FILEPATH = "/home/lcirne/scratch/mod_counts.json"
 
 num_iterations = 0
 
