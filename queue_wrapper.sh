6465ba1662d4d60b0f0d8ee4243c9d9245f6de94
lcirne <lcirne@uvm.edu>
2026-06-15 11:11:39 -0400

    Commenting out a cli command to remove some output after trial run.

--- queue_wrapper.sh
+++ queue_wrapper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --nodes=1                  # Number of nodes
-#SBATCH --time=24:00:00             # Wall clock time
+#SBATCH --time=48:00:00             # Wall clock time
 #SBATCH --ntasks-per-node 20
 #SBATCH --partition gpu
 
 python3 wrapper.py <scan_inputs.txt
 
-find . -mindepth 1 ! -name '*mm*' ! -name 'slurm*' ! -name 'run_job.sh' -exec rm -rf {} +
+#find . -mindepth 1 ! -name '*mm*' ! -name 'slurm*' ! -name 'run_job.sh' -exec rm -rf {} +
