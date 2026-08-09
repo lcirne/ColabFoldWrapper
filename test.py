6fa3d65fc63c48d17488db8be80d98dd8e8ceb4c
lcirne <lcirne@uvm.edu>
2026-08-09 19:15:33 -0400

    directory clean up.

--- test.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-if __name__=='__main__':
-    parser  = argparse.ArgumentParser()
-    parser.add_argument("--test", "-t",
-            default="1")
-    args = parser.parse_args()
-    print(args.test)
